10b570b69833c2d6a26c37c6a9a111736a766cb2
rohitkatore <rohit.krpud22@sinhgad.edu>
2025-01-31 14:40:32 +0530

    changes in bcrypt

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const userModel = require("../models/user.model");
 const { validationResult } = require("express-validator");
 
